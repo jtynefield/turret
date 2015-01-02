d5096a07b91ec7734c32e2afa8b5b100d3b1b3c9
John Tynefield <jtynefield@gmail.com>
2015-01-02 23:58:24 +0000

    basic 3-way comms working, building on jetson

--- jetson/sio.cpp
+++ jetson/sio.cpp
@@ -3,68 +3,49 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
 #include <string.h> // needed for memset
 
-/* 
-
+//  sudo i2cset -y 1 0x11 0x00 0xae
  
-#define BAUDRATE 9600
+#define I2CBUS "/dev/i2c-1"
+#define ARBOTIXDEV 0x11
 
-void setup() {
-  Serial.begin(BAUDRATE);
-}
-
-void loop() {
-  if ( Serial.available() > 0 ) {
-     int inByte = Serial.read();
-     inByte += 1;
-     Serial.write(inByte);
-  }
-}
-*/
- 
 int main(int argc,char** argv)
 {
-        struct termios tio;
-        struct termios stdio;
-        int tty_fd;
-        fd_set rdset;
- 
-        unsigned char c='D';
- 
-        printf("Please start with %s /dev/ttyS1 (for example)\n",argv[0]);
-        memset(&stdio,0,sizeof(stdio));
-        stdio.c_iflag=0;
-        stdio.c_oflag=0;
-        stdio.c_cflag=0;
-        stdio.c_lflag=0;
-        stdio.c_cc[VMIN]=1;
-        stdio.c_cc[VTIME]=0;
-        tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
-        tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
-        fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
- 
- 
- 
- 
-        memset(&tio,0,sizeof(tio));
-        tio.c_iflag=0;
-        tio.c_oflag=0;
-        tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
-        tio.c_lflag=0;
-        tio.c_cc[VMIN]=1;
-        tio.c_cc[VTIME]=5;
- 
-        tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
-        cfsetospeed(&tio,B9600);            // 115200 baud
-        cfsetispeed(&tio,B9600);            // 115200 baud
- 
-        tcsetattr(tty_fd,TCSANOW,&tio);
-        while (c!=27)
-        {
-                if (read(tty_fd,&c,1)>0)        write(STDOUT_FILENO,&c,1);              // if new data is available on the serial port, print it out
-                if (read(STDIN_FILENO,&c,1)>0)  write(tty_fd,&c,1);                     // if new data is available on the console, send it to the serial port
-        }
- 
-        close(tty_fd);
+    printf( "Opening i2c device /dev/i2c-1" );
+
+    int i2cdev;
+    const char *devname = I2CBUS;
+    if( (i2cdev = open(devname,O_RDWR)) < 0 ) {
+        printf( "failed to open the bus\n" );
+        exit( -1 );
+    }
+
+    if ( ioctl(i2cdev,I2C_SLAVE,ARBOTIXDEV) < 0 ) {
+        printf( "failed to acquire device/set target\n" );
+        exit( -1 );
+    }
+    printf( "setup success\n" );
+
+    struct termios stdio;
+    memset(&stdio,0,sizeof(stdio));
+    stdio.c_iflag=0;
+    stdio.c_oflag=0;
+    stdio.c_cflag=0;
+    stdio.c_lflag=0;
+    stdio.c_cc[VMIN]=1;
+    stdio.c_cc[VTIME]=0;
+    tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
+    tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
+    
+    unsigned char c=' ';
+    while (c!=27) {
+        if (read(STDIN_FILENO,&c,1)>0)  write(i2cdev,&c,1);                     // if new data is available on the console, send it to the serial port
+    }
+    close( i2cdev );
+    return 0; 
 }
+
